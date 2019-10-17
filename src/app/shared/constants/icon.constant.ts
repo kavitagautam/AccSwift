3c5effe4b54a4a8aa79533722cb8dda1614e49d9
Rabin <rabin.naga@bentraytech.com>
2019-10-17 12:08:46 +0545

    implement admin-lte template

---
+++ src/app/shared/constants/icon.constant.ts
@@ -0,0 +1,11 @@
+export const IconConst = {
+  DASHBOARD: "fas fa-fw fa-tachometer-alt",
+  SUSPECT: "fas fa-fw fa-user",
+  PROSPECT: "fas fa-users",
+  CLIENT: "fas fa-users",
+  ON_BOARD_CANDIDATE: "fas fa-users",
+  INVOICE_BILLING: "",
+  GENERATE_TOKENS: "",
+  EXPORT_INVOICE: "fa fa-upload",
+  TASK: "fa fa-tasks"
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseAPI:"http://accswift.bentrayhosting.com/",
+  baseAPI: "http://accswift.bentrayhosting.com/",
+  defaultImagePath: "./assets/dist/img",
   defaultImageUrl: "./assets/images/user.jpg"
 };
