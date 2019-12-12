393b73baafda08b0272c3d4e416564a9e66a1c6c
ChelSuch8 <such8888@gmail.com>
2019-12-12 14:30:56 +0545

    contraVoucher in progress

--- src/app/modules/contra-voucher/components/list-contra-voucher/list-contra-voucher.component.ts
+++ src/app/modules/contra-voucher/components/list-contra-voucher/list-contra-voucher.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ContraVoucherService } from "../../services/contra-voucher.service";
+import { Router } from "@angular/router";
+import { GridDataResult } from "@progress/kendo-angular-grid";
+import {
+  CompositeFilterDescriptor,
+  SortDescriptor
+} from "@progress/kendo-data-query";
+import { CashPaymentMaster } from "@app/modules/cash-payments/models/cash-payments.model";
+import { BsModalRef } from "ngx-bootstrap";
 
 @Component({
-  selector: 'app-list-contra-voucher',
-  templateUrl: './list-contra-voucher.component.html',
-  styleUrls: ['./list-contra-voucher.component.scss']
+  selector: "app-list-contra-voucher",
+  templateUrl: "./list-contra-voucher.component.html",
+  styleUrls: ["./list-contra-voucher.component.scss"]
 })
 export class ListContraVoucherComponent implements OnInit {
+  contraVoucherForm: FormGroup;
+  public gridView: GridDataResult;
+  listLoading: boolean;
+  contraVoucherMaster: CashPaymentMaster;
+  public filter: CompositeFilterDescriptor;
+  date: Date = new Date();
+  public pageSize = 10;
+  public skip = 0;
+  public currentPage = 1;
+  modalRef: BsModalRef;
+  constructor(
+    private fb: FormBuilder,
 
-  constructor() { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.editContraVoucherForm();
   }
 
+  editContraVoucherForm() {
+    this.contraVoucherForm = this.fb.group({
+      series: [""],
+      project: [""],
+      voucherNo: [""],
+      cashAccount: [""],
+      date: [""]
+    });
+  }
+
+  //Sorting Kendo Data
+  public allowUnsort = true;
+  public sort: SortDescriptor[] = [
+    {
+      field: "",
+      dir: "asc"
+    }
+  ];
+
+  //modal config to unhide when clicked outside
+  config = {
+    backdrop: true,
+    ignoreBackdropClick: true
+  };
+  // Date string parse
+  public currentYear = new Date().getFullYear();
+  public parseAdjust = (eventDate: Date): Date => {
+    const date = new Date(eventDate);
+    date.setFullYear(this.currentYear);
+    return date;
+  };
+
+  public sortChange(sort: SortDescriptor[]): void {
+    this.sort = sort;
+    this.getContraVoucherList();
+  }
+
+  getContraVoucherList() {}
 }
--- src/app/modules/contra-voucher/contra-voucher.module.ts
+++ src/app/modules/contra-voucher/contra-voucher.module.ts
@@ -5,6 +5,10 @@ import { ContraVoucherRoutingModule } from "./contra-voucher-routing.module";
 import { ListContraVoucherComponent } from "./components/list-contra-voucher/list-contra-voucher.component";
 import { EditContraVoucherComponent } from "./components/edit-contra-voucher/edit-contra-voucher.component";
 import { AddContraVoucherComponent } from "./components/add-contra-voucher/add-contra-voucher.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { GridModule, SharedModule } from "@progress/kendo-angular-grid";
+import { PopupModule } from "@progress/kendo-angular-popup";
+import { DropDownListModule } from "@progress/kendo-angular-dropdowns";
 
 @NgModule({
   declarations: [
@@ -12,6 +16,16 @@ import { AddContraVoucherComponent } from "./components/add-contra-voucher/add-c
     EditContraVoucherComponent,
     AddContraVoucherComponent
   ],
-  imports: [CommonModule, ContraVoucherRoutingModule]
+  imports: [
+    CommonModule,
+    ContraVoucherRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GridModule,
+    PopupModule,
+    DropDownListModule,
+    SharedModule
+  ]
 })
 export class ContraVoucherModule {}
