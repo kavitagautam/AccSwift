66582e92b73f95d2fcc169c91ee62bcde2db3dd3
ChelSuch8 <such8888@gmail.com>
2019-12-12 17:44:30 +0545

    ContraVoucher in progress

--- src/app/modules/contra-voucher/components/edit-contra-voucher/edit-contra-voucher.component.ts
+++ src/app/modules/contra-voucher/components/edit-contra-voucher/edit-contra-voucher.component.ts
@@ -1,15 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContraVoucherService } from './../../services/contra-voucher.service';
+import { formatDate } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormArray, Validators } from "@angular/forms";
+import { ContraVoucherMaster } from "../models/contravoucher.model";
 
 @Component({
-  selector: 'app-edit-contra-voucher',
-  templateUrl: './edit-contra-voucher.component.html',
-  styleUrls: ['./edit-contra-voucher.component.scss']
+  selector: "app-edit-contra-voucher",
+  templateUrl: "./edit-contra-voucher.component.html",
+  styleUrls: ["./edit-contra-voucher.component.scss"]
 })
 export class EditContraVoucherComponent implements OnInit {
+  editContraVoucherForm: FormGroup;
+  contraVoucherMaster: ContraVoucherMaster;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private contraVoucherService: ContraVoucherService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getEditContraVoucherForm();
+    this.contraVoucherService.init();
+    this.getParambyId();
+  }
+
+  getEditContraVoucherForm() {
+    this.editContraVoucherForm = this.fb.group({
+      series: [this.contraVoucherMaster ? this.contraVoucherMaster.SeriesID : ""],
+      project: [this.contraVoucherMaster ? this.contraVoucherMaster.ProjectID : ""],
+      voucherNo: [this.contraVoucherMaster ? this.contraVoucherMaster.VoucherNo : ""],
+      cashAccount: [this.contraVoucherMaster ? this.contraVoucherMaster.LedgerID : ""],
+      cashParty: [this.contraVoucherMaster ? this.contraVoucherMaster : ""],
+      date: [this.contraVoucherMaster ? formatDate(this.contraVoucherMaster.CreatedDate, "yyyy-MM-dd", "en-US") : ""],
+      contraVoucherEntryList: this.fb.array([this.addContraVoucherEntryFormGroup()])
+    });
+  }
+
+  // Building FormArray ......
+  addContraVoucherEntryFormGroup(): FormGroup {
+    return this.fb.group({
+      ledgerCode: [''],
+      particularsOraccountingHead: [''],
+      voucherNo: [''],
+      amount: [''],
+      currentBalance: [''],
+      vType: [''],
+      remarks: [''],
+    })
+  }
+
+  getParambyId() {
+    this.route.paramMap.subscribe(params => {
+      const paramGetId = params.get('id');
+      if (paramGetId) {
+        this.contraVoucherService.getCashReceiptDetails(paramGetId).subscribe(res => {
+          this.contraVoucherMaster = res;
+          this.getEditContraVoucherForm();
+          // this.setCashReceiptList();
+        })
+      }
+    })
+  }
+
+  get getContraVoucherEntryList(): FormArray {
+    return <FormArray>this.editContraVoucherForm.get("contraVoucherEntryList");
+  }
+
+
+  // setCashReceiptList(): void {
+  //   this.editContraVoucherForm.setControl(
+  //     "contraVoucherEntryList",
+  //     this.setContraVoucherFormArray(this.cashReceiptDetails.CashReceiptDetails)
+  //   );
+
+  setContraVoucherFormArray(cashRecepitDetails): FormArray {
+    const contraVoucherFormArray = new FormArray([]);
+    if (cashRecepitDetails && cashRecepitDetails.length > 0) {
+      cashRecepitDetails.forEach(element => {
+        contraVoucherFormArray.push(
+          this.fb.group({
+            ledgerCode: [element.Ledger.Code ? element.Ledger.Code : ""],
+            particularsOraccountingHead: [
+              element.Ledger.EngName,
+              Validators.required
+            ],
+            voucherNo: element.VoucherNumber,
+            amount: element.Amount,
+            currentBalance: element.Amount,
+            vType: element.VoucherType,
+            remarks: element.Remarks
+          })
+        );
+      });
+    } else {
+      contraVoucherFormArray.push(
+        this.fb.group({
+          particularsOraccountingHead: ["", Validators.required],
+          voucherNo: [""],
+          amount: [""],
+          currentBalance: [""],
+          vType: [""],
+          remarks: [""]
+        })
+      );
+    }
+    return contraVoucherFormArray;
   }
 
 }
--- src/app/modules/contra-voucher/components/list-contra-voucher/list-contra-voucher.component.ts
+++ src/app/modules/contra-voucher/components/list-contra-voucher/list-contra-voucher.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { ContraVoucherService } from "../../services/contra-voucher.service";
 import { Router } from "@angular/router";
-import { GridDataResult } from "@progress/kendo-angular-grid";
+import { GridDataResult, PageChangeEvent } from "@progress/kendo-angular-grid";
 import {
   CompositeFilterDescriptor,
   SortDescriptor
 } from "@progress/kendo-data-query";
-import { CashPaymentMaster } from "@app/modules/cash-payments/models/cash-payments.model";
-import { BsModalRef } from "ngx-bootstrap";
+import { BsModalRef, BsModalService } from "ngx-bootstrap";
+import { ContraVoucherMaster } from "../models/contravoucher.model";
+import { ToastrService } from "ngx-toastr";
+import { ConfirmationDialogComponent } from "@app/shared/component/confirmation-dialog/confirmation-dialog.component";
 
 @Component({
   selector: "app-list-contra-voucher",
@@ -19,17 +21,20 @@ export class ListContraVoucherComponent implements OnInit {
   contraVoucherForm: FormGroup;
   public gridView: GridDataResult;
   listLoading: boolean;
-  contraVoucherMaster: CashPaymentMaster;
+  contraVoucherMaster: ContraVoucherMaster;
   public filter: CompositeFilterDescriptor;
   date: Date = new Date();
   public pageSize = 10;
   public skip = 0;
   public currentPage = 1;
   modalRef: BsModalRef;
+  cashList: ContraVoucherMaster[];
   constructor(
     private fb: FormBuilder,
-
-    private router: Router
+    private contraVoucherService: ContraVoucherService,
+    private router: Router,
+    private modalService: BsModalService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit() {
@@ -73,5 +78,107 @@ export class ListContraVoucherComponent implements OnInit {
     this.getContraVoucherList();
   }
 
-  getContraVoucherList() {}
+  getContraVoucherList(): void {
+    this.listLoading = true;
+    const params = {
+      PageNo: this.currentPage,
+      DisplayRow: this.pageSize,
+      OrderBy: "",
+      Direction: "asc" // "asc" or "desc"
+    };
+
+    this.contraVoucherService.getCashReceiptMaster().subscribe(
+      res => {
+        this.listLoading = true;
+
+        //mapping the data to change string date format to Date
+        const sampleData = res.map(
+          dataItem =>
+            <ContraVoucherMaster>{
+              IsPayByInvoice: dataItem.IsPayByInvoice,
+              TotalAmount: dataItem.TotalAmount,
+              CashReceiptDetails: dataItem.CashReceiptDetails,
+              LedgerID: dataItem.LedgerID,
+              LedgerName: dataItem.LedgerName,
+              ID: dataItem.ID,
+              SeriesID: dataItem.SeriesID,
+              SeriesName: dataItem.SeriesName,
+              VoucherNo: dataItem.VoucherNo,
+              Date: this.parseAdjust(dataItem.Date),
+              ProjectID: dataItem.ProjectID,
+              ProjectName: dataItem.ProjectName,
+              Fields: {
+                Field1: dataItem.Fields.Field1,
+                Field2: dataItem.Fields.Field2,
+                Field3: dataItem.Fields.Field3,
+                Field4: dataItem.Fields.Field4,
+                Field5: dataItem.Fields.Field5
+              },
+              Remarks: dataItem.Remarks,
+              CreatedBy: dataItem.CreatedBy,
+              CreatedDate: this.parseAdjust(dataItem.CreatedDate),
+              ModifiedBy: dataItem.ModifiedBy,
+              ModifiedDate: this.parseAdjust(dataItem.ModifiedDate)
+            }
+        );
+        this.cashList = sampleData;
+        this.gridView = {
+          data: this.cashList,
+          total: this.cashList ? this.cashList.length : 0
+        };
+      },
+      error => {
+        this.listLoading = false;
+      },
+      () => {
+        this.listLoading = false;
+      }
+    );
+  }
+
+  public searchForm() {
+    this.getContraVoucherList();
+  }
+
+  public filterChange(filter): void {
+    this.filter = filter;
+  }
+
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+    if (event.skip == 0) {
+      this.skip = event.skip;
+      this.currentPage = 1;
+    } else {
+      this.skip = event.skip;
+      const pageNo = event.skip / event.take + 1;
+      this.currentPage = pageNo;
+    }
+    this.getContraVoucherList();
+  }
+
+  public edit(): void {
+    this.router.navigate(["/contra/edit"]);
+  }
+
+  openConfirmationDialogue(dataItem) {
+    const contraId = {
+      id: dataItem.ID
+    };
+    this.modalRef = this.modalService.show(
+      ConfirmationDialogComponent,
+      this.config
+    );
+    this.modalRef.content.data = "Receipt No." + dataItem.VoucherNo;
+    this.modalRef.content.action = "delete";
+    this.modalRef.content.onClose.subscribe(confirm => {
+      if (confirm) {
+        this.deleteContraById(contraId.id);
+      }
+    });
+  }
+
+  public deleteContraById(id): void {
+    this.toastr.success("Voucher deleted succesfully");
+  }
 }
