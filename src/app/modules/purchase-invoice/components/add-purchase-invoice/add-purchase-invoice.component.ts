60d67167ce8ed3273006ab8280947e68a7229fa9
ChelSuch8 <such8888@gmail.com>
2019-12-25 23:24:46 +0545

    WIP for Purchase Invoice

--- src/app/modules/purchase-invoice/components/add-purchase-invoice/add-purchase-invoice.component.ts
+++ src/app/modules/purchase-invoice/components/add-purchase-invoice/add-purchase-invoice.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { PurchaseInvoiceService } from "./../../services/purchase-invoice.service";
+import { FormBuilder } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-purchase-invoice',
-  templateUrl: './add-purchase-invoice.component.html',
-  styleUrls: ['./add-purchase-invoice.component.scss']
+  selector: "app-add-purchase-invoice",
+  templateUrl: "./add-purchase-invoice.component.html",
+  styleUrls: ["./add-purchase-invoice.component.scss"]
 })
 export class AddPurchaseInvoiceComponent implements OnInit {
-
-  constructor() { }
+  addPurchaseForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private purchaseInvoiceService: PurchaseInvoiceService
+  ) {}
 
   ngOnInit() {
+    this.buildListPurchaseInvoiceForm();
   }
 
+  buildListPurchaseInvoiceForm() {
+    this.addPurchaseForm = this.fb.group({
+      seriesName: "",
+      cashParty: "",
+      purchaseAc: "",
+      voucherNo: "",
+      partyBillNo: "",
+      depot: "",
+      project: "",
+      date: "",
+      orderNo: "",
+      remarks: ""
+    });
+  }
 }
--- src/app/modules/purchase-invoice/components/edit-purchase-invoice/edit-purchase-invoice.component.ts
+++ src/app/modules/purchase-invoice/components/edit-purchase-invoice/edit-purchase-invoice.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-edit-purchase-invoice',
-  templateUrl: './edit-purchase-invoice.component.html',
-  styleUrls: ['./edit-purchase-invoice.component.scss']
+  selector: "app-edit-purchase-invoice",
+  templateUrl: "./edit-purchase-invoice.component.html",
+  styleUrls: ["./edit-purchase-invoice.component.scss"]
 })
 export class EditPurchaseInvoiceComponent implements OnInit {
-
-  constructor() { }
+  editPurchaseForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.buildEditPurchaseInvoiceForm();
   }
 
+  buildEditPurchaseInvoiceForm() {
+    this.editPurchaseForm = this.fb.group({
+      seriesName: "",
+      cashParty: "",
+      purchaseAc: "",
+      voucherNo: "",
+      partyBillNo: "",
+      depot: "",
+      project: "",
+      date: "",
+      orderNo: "",
+      remarks: ""
+    });
+  }
 }
