920fdff238f76a72975ef9cb9e27500359b13180
Santosh Thapa <santosh.thapa@bentraytech.com>
2020-01-01 16:15:13 +0545

    Purchase Order list page layout designed

---
+++ src/app/modules/purchase-order/models/purchase-order.model.ts
@@ -0,0 +1,24 @@
+
+export interface ProjectList {
+    ID: number;
+    ProjectNumber: number;
+    ParentProjectID: number;
+    EngName: string;
+    NepName: string;
+    Description: string;
+    CreatedBy: string;
+    CreatedDate: string;
+    ModifiedBy: string;
+    ModifiedDate: string;
+  }
+  
+  export interface SeriesList {
+    ID: number;
+    EngName: string;
+    NepName: string;
+    VoucherType: string;
+    AutoNumber: number;
+    BuiltIn: boolean;
+  }
+  
+  
--- src/app/modules/purchase-order/services/purchase-order.service.ts
+++ src/app/modules/purchase-order/services/purchase-order.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { ProjectList } from '../models/purchase-order.model';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientService } from '@app/core/services/http-client/http-client.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseOrderService {
+  _api_URL = environment.baseAPI;
+  projectList: ProjectList;
+  constructor(
+    private http: HttpClient,
+    private httpService: HttpClientService
+  ) {
+    this.getProjectLists();
+   }
 
-  constructor() { }
+  getProjectLists(): void {
+    this.httpService
+      .get(`${this._api_URL}project`)
+      .subscribe((res: ProjectList) => {
+        this.projectList = res;
+      });
+  }
+
+  getPurchaseOrderMaster(){
+    return this.httpService.get(
+      `${this._api_URL}PurchaseInvoiceMaster`
+    );
+  }
+
+  getPurchaseOrderDetails(id: any)  {
+    return this.httpService.get(`${this._api_URL}PurchaseInvoiceMaster/${id}`);
+  }
 }
