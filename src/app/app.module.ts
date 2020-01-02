1f5f076455f6a1b307b96394b7c43b3d3dc63a2d
Santosh Thapa <santosh.thapa@bentraytech.com>
2020-01-02 15:30:17 +0545

    Switching between Locale and the bs Datepiceker Css file added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-// import "@progress/kendo-angular-intl/locales/ne/all"; // For Kendo Nepali Input
-import "../../lib/ne/all"; //Customize locales for location Nepal fro kendo input
-import { registerLocaleData } from "@angular/common";
-import localeNe from "@angular/common/locales/ne";
 import { HttpInterceptorsService } from "./core/services/http-interceptors/http-interceptors.service";
-registerLocaleData(localeNe, "ne");
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CoreModule, AppRoutingModule, HttpClientModule],
@@ -21,10 +15,8 @@ registerLocaleData(localeNe, "ne");
       useClass: HttpInterceptorsService,
       multi: true
     },
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: LOCALE_ID, useValue: "ne" }
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
-
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
