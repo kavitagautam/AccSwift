7aa434518d93c732ec464ef7ad141d74755483ba
ChelSuch8 <such8888@gmail.com>
2020-01-27 12:49:31 +0545

    Depot module added

---
+++ src/app/modules/depot/depot.module.ts
@@ -0,0 +1,31 @@
+import { InputsModule } from "@progress/kendo-angular-inputs";
+import { DropDownsModule } from "@progress/kendo-angular-dropdowns";
+import { DropDownListModule } from "@progress/kendo-angular-dropdowns";
+import { PopupModule } from "@progress/kendo-angular-popup";
+import { GridModule } from "@progress/kendo-angular-grid";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { DepotRoutingModule } from "./depot-routing.module";
+
+import { EditDepotComponent } from "./components/edit-depot/edit-depot.component";
+import { ListDepotComponent } from "./components/list-depot/list-depot.component";
+import { AddDepotComponent } from "./components/add-depot/add-depot.component";
+
+@NgModule({
+  declarations: [AddDepotComponent, EditDepotComponent, ListDepotComponent],
+  imports: [
+    CommonModule,
+    DepotRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GridModule,
+    PopupModule,
+    DropDownListModule,
+    DropDownsModule,
+    InputsModule
+  ]
+})
+export class DepotModule {}
