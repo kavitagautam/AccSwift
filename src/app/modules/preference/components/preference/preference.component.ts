7bba22da94f7826e209b1d5d4baf139754bdf266
Sachet <git@gitlab.com:santosTT/AccSwift.git>
2020-03-10 22:16:16 +0545

    Preference module is on progress

--- src/app/modules/preference/components/preference/preference.component.ts
+++ src/app/modules/preference/components/preference/preference.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./preference.component.scss"]
 })
 export class PreferenceComponent implements OnInit {
-  constructor() {}
+  toggler = document.getElementsByClassName("boxes");
+  nodeName: any;
 
-  ngOnInit() {}
+  constructor() {}
+  public i: any;
+  ngOnInit() {
+    this.treeView();
+  }
+
+  selectedNode(item): void {
+    this.nodeName = item;
+  }
+
+  treeView() {
+    for (this.i = 0; this.i < this.toggler.length; this.i++) {
+      this.toggler[this.i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("check-boxes");
+      });
+    }
+  }
 }
