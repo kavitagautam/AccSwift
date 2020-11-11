91841e9ab45320d92785421bd2038bdacaab808b
Santosh Thapa <santosh.thapa@bentraytech.com>
2020-11-11 13:47:59 +0545

    Pos calculation fixes

--- src/app/modules/pos/components/pos/pos.component.ts
+++ src/app/modules/pos/components/pos/pos.component.ts
@@ -216,6 +216,7 @@ export class PosComponent implements OnInit {
 
   deleteProduct(index): void {
     this.productList.splice(index, 1);
+    this.calculateTotal(this.productList);
   }
 
   discountAmountChange(): void {
@@ -239,7 +240,9 @@ export class PosComponent implements OnInit {
         selectedProduct[0].Quantity = selectedProduct[0].Quantity + 1;
         selectedProduct[0].Amount =
           selectedProduct[0].Quantity * selectedProduct[0].SalesRate;
-
+        selectedProduct[0].NetAmount =
+          selectedProduct[0].Quantity * selectedProduct[0].SalesRate -
+          this.discountItem;
         selectedProduct[0].TaxAmount =
           selectedProduct[0].Quantity *
           selectedProduct[0].SalesRate *
@@ -301,21 +304,52 @@ export class PosComponent implements OnInit {
     };
 
     // console.log("Comment" + JSON.stringify(obj));
-    this.posServices.addSalesInvoice(obj).subscribe(
-      (response) => {
-        console.log("response" + JSON.stringify(response));
+    this.posServices.addSalesInvoice(obj).subscribe((response) => {
+      console.log("response" + JSON.stringify(response));
+      this.posServices.getPDF(response.Entity.ID).subscribe(
+        (response) => {
+          var newBlob = new Blob([response], { type: "application/pdf" });
+          console.log("response " + JSON.stringify(response));
+          // IE doesn't allow using a blob object directly as link href
+          // instead it is necessary to use msSaveOrOpenBlob
+          if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(newBlob);
+            return;
+          }
 
-        // setTimeout(() => {
-        //   window.location.reload();
-        // }, 300);
-      },
-      (error) => {
-        this.toastr.error(JSON.stringify(error.error.Message));
-      },
-      () => {
-        this.toastr.success("Invoice added successfully");
-      }
-    );
+          // For other browsers:
+          // Create a link pointing to the ObjectURL containing the blob.
+          const data = window.URL.createObjectURL(newBlob);
+
+          var link = document.createElement("a");
+          link.href = data;
+          link.download = "Je kar.pdf";
+          // this is necessary as link.click() does not work on the latest firefox
+          link.dispatchEvent(
+            new MouseEvent("click", {
+              bubbles: true,
+              cancelable: true,
+              view: window,
+            })
+          );
+
+          setTimeout(function () {
+            // For Firefox it is necessary to delay revoking the ObjectURL
+            window.URL.revokeObjectURL(data);
+            link.remove();
+          }, 100);
+        },
+        (error) => {
+          this.toastr.error(JSON.stringify(error.error.Message));
+        },
+        () => {
+          this.toastr.success("Invoice added successfully");
+        }
+      );
+      // setTimeout(() => {
+      //   window.location.reload();
+      // }, 300);
+    });
   }
 
   openCommit(template: TemplateRef<any>): void {
