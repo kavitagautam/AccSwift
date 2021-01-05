232fa21827444b6b389aec527e830377934bd745
kaveeta <kavitagautam014@gmail.com>
2021-01-05 20:52:25 +0545

    add-budget-layout

--- src/app/modules/budget/components/budget-setup/budget-setup.component.ts
+++ src/app/modules/budget/components/budget-setup/budget-setup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'budget-setup',
-  templateUrl: './budget-setup.component.html',
+  templateUrl: '../common-html/common-budget-setup.html',
   styleUrls: ['./budget-setup.component.scss']
 })
 export class BudgetSetupComponent implements OnInit {
