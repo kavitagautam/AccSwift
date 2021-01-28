f1bab8595542bac4ff85ed85dc8ab72e5c82f88d
kaveeta <kavitagautam014@gmail.com>
2021-01-28 13:19:59 +0545

    budget module

--- src/app/modules/budget/budget.module.ts
+++ src/app/modules/budget/budget.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { GridModule } from "@progress/kendo-angular-grid";
 import { PopupModule } from "@progress/kendo-angular-popup";
 import { DropDownListModule } from "@progress/kendo-angular-dropdowns";
@@ -9,19 +10,24 @@ import { LayoutModule } from '@progress/kendo-angular-layout';
 import { LabelModule } from '@progress/kendo-angular-label';
 import { InputsModule } from '@progress/kendo-angular-inputs';
 import { BudgetRoutingModule } from "./budget-routing.module";
+import { AccswiftSharedModule } from "../accswift-shared/accswift-shared.module";
 import { BudgetSetupComponent } from "./components/budget-setup/budget-setup.component";
 import { BudgetAllocationComponent } from "./components/budget-allocation/budget-allocation.component";
 import { ViewBudgetComponent } from "./components/view-budget/view-budget.component";
+import { BudgetAllocationMastersComponent } from './components/budget-allocation-masters/budget-allocation-masters.component';
 
 @NgModule({
   declarations: [
     BudgetSetupComponent,
     BudgetAllocationComponent,
     ViewBudgetComponent,
+    BudgetAllocationMastersComponent,
     
   ],
-  imports: [CommonModule, BudgetRoutingModule, TreeViewModule, DropDownsModule, LayoutModule, LabelModule, InputsModule, GridModule,
-  PopupModule, DropDownListModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, BudgetRoutingModule, TreeViewModule, DropDownsModule, LayoutModule, LabelModule, InputsModule, GridModule,
+  PopupModule, DropDownListModule, AccswiftSharedModule],
+
+  entryComponents:[ BudgetSetupComponent ],
   
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/app/modules/budget/components/budget-setup/budget-setup.component.ts
+++ src/app/modules/budget/components/budget-setup/budget-setup.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges,
+  SimpleChange, } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+import { BudgetMinListView, BudgetMinListViewRootModel, BudgetDetails, BudgetDetailsRootModel
+  } from "@accSwift-modules/budget/models/budget-model";
+import { BudgetService } from "../../services/budget.service";
+import { ToastrService } from "ngx-toastr";
+import { BsModalRef, BsModalService } from "ngx-bootstrap";
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'budget-setup',
@@ -6,10 +14,266 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-setup.component.scss']
 })
 export class BudgetSetupComponent implements OnInit {
+  @Input("selectedItem") selectedItem;
+  selectedBudgetId: number;
+  BudgetMinListView: any;
+  BudgetDetails: BudgetDetails;
+  budgetForm: FormGroup;
+  editMode: boolean;
+  addMode: boolean;
+  modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(
+    private BudgetService: BudgetService,
+    public _fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    private modalService: BsModalService
+  ) {
+    
+   }
 
   ngOnInit() {
+    this.buildBudgetForm();
+    this.getBudget();
+      this.addMode = true;
+      if (this.selectedItem == null) {
+        this.editMode = false;
+        this.addMode = true;
+        this.addBudget();
+      } else {
+        this.editMode = true;
+        this.addMode = false;
+        this.getBudgetDetails();
+      }
   }
+
+  getBudget(): void {
+    this.BudgetService.getBudgetMinListView().subscribe((response) => {
+      this.BudgetMinListView = response.Entity;
+    });
+  }
+
+  getBudgetDetails(): void {
+    this.BudgetService
+      .getBudgetDetails(this.selectedItem.ID)
+      .subscribe((res) => {
+        this.BudgetDetails = res.Entity;
+        this.setBudgetAllocationDetails();
+        this.setBudgetAllocationMasters();
+        this.budgetForm.patchValue(this.BudgetDetails);
+        this.selectedBudgetId = this.BudgetDetails.ID;
+      });
+  }
+
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+    let log: string[] = [];
+    for (let p in changes) {
+      let c = changes[p];
+      this.selectedItem = c.currentValue;
+      if (this.selectedItem) {
+        this.selectedBudgetId = this.selectedItem.ID;
+        if (this.selectedBudgetId && this.selectedItem.TypeOf == 1) {
+          this.editMode = true;
+          this.addMode = false;
+          this.getBudgetDetails();
+        } 
+      }
+    }
+  }
+
+  buildBudgetForm(): void{
+    this.budgetForm = this._fb.group({
+      ID: [this.BudgetMinListView ? this.BudgetMinListView.ID : null],
+      Name: [
+        this.BudgetMinListView ? this.BudgetMinListView.Name : "",
+        Validators.required,
+      ],
+      StartDate: [
+        this.BudgetMinListView ? this.BudgetMinListView.StartDate : "",
+        Validators.required,
+      ],
+      EndDate: [
+        this.BudgetMinListView ? this.BudgetMinListView.EndDate : "",
+        Validators.required,
+      ],
+      Remarks: [
+        this.BudgetDetails ? this.BudgetDetails.Remarks : "",
+        Validators.required,
+      ],
+      BudgetAllocationMasters: this._fb.array([this.budgetAllocationMastersFormGroup()])
+    })
+  }
+
+  get getBudgetAllocationDetails(): FormArray {
+    return <FormArray>this.budgetForm.get("BudgetAllocationDetails");
+  }
+
+  get getBudgetAllocationMasters(): FormArray {
+    return <FormArray>this.budgetForm.get("BudgetAllocationMasters");
+  }
+
+
+  budgetAllocationMastersFormGroup(): FormGroup {
+    return this._fb.group({
+      ID: [null],
+      BudgetID: [null],
+      AccountID: [null],
+      AccountName: "",
+      AccountType: "",
+      AllocationAmount: [null],
+      BudgetAllocationDetails: this._fb.array([this.budgetAllocationDetailsFormGroup()])
+    })
+  }
+
+  budgetAllocationDetailsFormGroup(): FormGroup {
+    return this._fb.group(
+      {
+        ID: [null],
+        BudgetMasterID: [null],
+        AccClassID: [null],
+        AccClassName: "",
+        Amount: [null]
+      }
+    )
+  }
+
+  setBudgetAllocationDetails(): void {
+    this.budgetForm.setControl(
+      "BudgetAllocationDetails",
+      this.setBudgetAllocationDetailsArray(this.BudgetDetails.BudgetAllocationMasters[1].BudgetAllocationDetails)
+    );
+  }
+
+  setBudgetAllocationMasters(): void {
+    this.budgetForm.setControl(
+      "BudgetAllocationMasters",
+      this.setBudgetAllocationMastersArray(this.BudgetDetails.BudgetAllocationMasters)
+    );
+  }
+
+  // this block of code is used to show form array data in the template.....
+  setBudgetAllocationDetailsArray(budgetAllocationDetails): FormArray {
+    const detailsList = new FormArray([]);
+    if (budgetAllocationDetails && budgetAllocationDetails.length > 0) {
+      budgetAllocationDetails.forEach((element) => {
+        detailsList.push(
+          this._fb.group({
+            ID: [element.ID],
+            BudgetMasterID: [element.BudgetMasterID],
+            AccClassID: [element.AccClassID],
+            AccClassName: [element.AccClassName],
+            Amount: [element.Amount]
+          })
+        );
+      });
+    } else {
+      detailsList.push(
+        this._fb.group({
+          ID: [null],
+          BudgetMasterID: [null],
+          AccClassID: [null],
+          AccClassName: [""],
+          Amount: [null]
+        })
+      );
+    }
+    return detailsList;
+  }
+
+  // this block of code is used to show form array data in the template.....
+  setBudgetAllocationMastersArray(budgetAllocationMasters): FormArray {
+    const mastersList = new FormArray([]);
+    if (budgetAllocationMasters && budgetAllocationMasters.length > 0) {
+      budgetAllocationMasters.forEach((element) => {
+        mastersList.push(
+          this._fb.group({
+            ID: [element.ID],
+            BudgetID: [element.BudgetID],
+            AccountID: [element.AccountID],
+            AccountName: [element.AccountName],
+            AccountType: [element.AccountType],
+            AllocationAmount: [element.AllocationAmount],
+            BudgetAllocationDetails: this.setBudgetAllocationDetailsArray(
+              element.BudgetAllocationDetails
+            )
+          })
+        );
+      });
+    } else {
+      for (let i = 0; i < 5; i++) {
+        mastersList.push(
+          this._fb.group({
+            ID: [null],
+            BudgetID: [null],
+            AccountID: [null],
+            AccountName: [""],
+            AccountType: [""],
+            AllocationAmount: [null],
+            BudgetAllocationDetails: this._fb.array([this.budgetAllocationDetailsFormGroup()]),
+          })
+        );
+      }
+    }
+    return mastersList;
+  }
+
+  public saveBudget(): void {
+    if (this.addMode) {
+      if (this.budgetForm.invalid) return;
+
+      this.BudgetService.addBudget(this.budgetForm.value).subscribe(
+        (response) => {
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        },
+        (error) => {
+          this.toastr.error(JSON.stringify(error.error.Message));
+        },
+        () => {
+          this.toastr.success("Budget added successfully");
+        }
+      );
+    } else {
+      if (this.budgetForm.invalid) return;
+
+      this.BudgetService
+        .updateBudget(this.budgetForm.value)
+        .subscribe(
+          (response) => {
+            setTimeout(() => {
+              window.location.reload();
+            }, 1000);
+          },
+          (error) => {
+            this.toastr.error(JSON.stringify(error.error.Message));
+          },
+          () => {
+            this.toastr.success("Budget edited successfully");
+          }
+        );
+    }
+  }
+
+  public cancelBudget(): void {
+    this.BudgetMinListView = null;
+    this.buildBudgetForm();
+  }
+
+  addBudget(): void {
+    this.addMode = true;
+    this.editMode = false;
+    this.budgetForm.reset();
+    this.budgetForm
+        .get("ID")
+        .setValue(
+          this.BudgetMinListView ? this.BudgetMinListView.ID : null
+        );
+    this.BudgetMinListView = null;
+  }
+
+  
 
 }
