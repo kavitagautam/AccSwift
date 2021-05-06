5d49940766c2690c7a8342204a508d23fefe3a9a
kaveeta <kavitagautam014@gmail.com>
2021-05-06 19:21:07 +0545

    API Integration for Product and Client Name

--- src/app/modules/delivery-notes/components/add-delivery-notes/add-delivery-notes.component.ts
+++ src/app/modules/delivery-notes/components/add-delivery-notes/add-delivery-notes.component.ts
@@ -1,8 +1,14 @@
+import { AddProductComponent } from '@accSwift-modules/accswift-shared/components/add-product/add-product/add-product.component';
+import { DetailsEntryGridService } from '@accSwift-modules/accswift-shared/services/details-entry-grid/details-entry-grid.service';
 import { DeliveryNotes } from '@accSwift-modules/delivery-notes/models/delivery-notes.model';
 import { DeliveryNotesService } from '@accSwift-modules/delivery-notes/services/delivery-notes.service';
+import { LedgerMin } from '@accSwift-modules/ledger/models/ledger.models';
+import { ProductMin } from '@accSwift-modules/product/models/product-min.model';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -15,12 +21,24 @@ export class AddDeliveryNotesComponent implements OnInit {
   public deliveryNotesForm: FormGroup;
   listLoading: boolean;
   deliveryNotes: DeliveryNotes[];
+  modalRef: BsModalRef;
   productArray;
+  config = {
+    backdrop: true,
+    ignoreBackdropClick: true,
+    centered: true,
+    class: "modal-lg",
+  };
+  public productList: ProductMin[] = [];
+  public ledgerList: LedgerMin[] = [];
 
   constructor(
     private _fb: FormBuilder,
     private router: Router,
     private deliveryNotesService: DeliveryNotesService,
+    public gridServices: DetailsEntryGridService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
     ) { }
 
   ngOnInit() {
@@ -28,7 +46,12 @@ export class AddDeliveryNotesComponent implements OnInit {
     this.productArray = <FormArray>(
         this.deliveryNotesForm.get("DeliveryProductsList")
       );
-    console.log(this.productArray.at(0).value)
+      this.gridServices.getProductDD().subscribe((response) => {
+        this.productList = response.Entity;
+      });
+      this.gridServices.getLedgerDD().subscribe((response) => {
+        this.ledgerList = response.Entity;
+      });
   }
 
   buildDeliveryNotesForm(): void {
@@ -76,17 +99,38 @@ export class AddDeliveryNotesComponent implements OnInit {
     // );
     // console.log(productArray.at(0).value)
     // productArray.controls[index].get("ProductID").setValue(13681);
+    if (this.deliveryNotesForm.invalid) return;
     this.deliveryNotesService.getDeliveryNotes(this.deliveryNotesForm.value).subscribe(
       (response) => {
         if (response)
         {
-          
+          this.router.navigate(["/delivery-notes"]);
         }
       },
       (error) => {
+        this.toastr.error(JSON.stringify(error.error.Message));
       },
       () => {
+        this.toastr.success("Delivery Notes added successfully");
       }
+    );
+  }
+
+  productDDFilter(value, i): void {
+    this.productList = this.gridServices.productList.filter(
+      (s) => s.CodeName.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
+    const selectedTaxValue = this.gridServices.productList.filter(
+      (s) => s.ProductID === value
+    );
+  }
+
+  ledgerDDFilter(value): void {
+    this.ledgerList = this.gridServices.ledgerList.filter(
+      (s) => s.CodeName.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
+    const selectedTaxValue = this.gridServices.ledgerList.filter(
+      (s) => s.LedgerID === value
     );
   }
 
