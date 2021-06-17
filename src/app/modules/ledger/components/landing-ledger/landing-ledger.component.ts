5ec30f48113e6094e48ad224d89d48fd38475c05
kaveeta <kavitagautam014@gmail.com>
2021-06-17 09:37:10 +0545

    Filter methods and Recursive Function for Ledger

--- src/app/modules/ledger/components/landing-ledger/landing-ledger.component.ts
+++ src/app/modules/ledger/components/landing-ledger/landing-ledger.component.ts
@@ -33,8 +33,12 @@ export class LandingLedgerComponent implements OnInit {
   listViewLoading: boolean;
   ledgerGroupList: any; 
   userType: string = localStorage.getItem("user_type");
-  ledgerAssets:string = JSON.parse(localStorage.getItem("LedgerAssets"));
-  ledgerLiab:string = JSON.parse(localStorage.getItem("LedgerLiab"));
+  ledgerData: any;
+  assetsData: any;
+  liabilityData: any;
+  incomeData: any;
+  expenditureData: any;
+  
 
   //Expanding the tree view
   public expandedKeys: any[] = ["Assets", "Current Assets", "Banks"];
@@ -49,6 +53,7 @@ export class LandingLedgerComponent implements OnInit {
     this.selectedGroupTab = true;
     this.loadLedgerTreeView();
     this.loadLedgerGroupList();
+    this.groupLedgerTreeItems();
   }
 
   loadLedgerTreeView(): void {
@@ -70,36 +75,129 @@ export class LandingLedgerComponent implements OnInit {
 
   }
 
-  assetsFilter(sortedArray) {
-    var ledgerTree = JSON.parse(localStorage.getItem("LedgerTreeList"));
+
+  groupLedgerTreeItems(){
+    const ledgerTree = JSON.parse(localStorage.getItem("LedgerTreeList"));
+    this.ledgerData = ledgerTree;
     console.log(ledgerTree);
-    sortedArray = ledgerTree.filter(function(val)
-    {
-      console.log(val.Code);
-      console.log(val.GroupOrLedgerNo);
-      console.log(val.Child);
-      if (val.Code=="fgfg" &&
-      val.GroupOrLedgerNo == 1)
-      return sortedArray;
-    });
-    localStorage.setItem("LedgerAssets", JSON.stringify(sortedArray));
-    return sortedArray;
+    let assets,liabilities,income,expenditure;
+    ledgerTree.map(function(item){
+      console.log(item);
+      if (item.Title==="Assets"){
+        assets=item;
+        console.log(item.Child)
+      }
+      else if (item.Title==="Liabilities"){
+        liabilities=item;
+      }
+      else if (item.Title==="Income"){
+        income=item;
+      }
+      else if(item.Title==="Expenditure"){
+        expenditure=item;
+      }
+    })
+    this.assetsData = assets;
+    this.liabilityData = liabilities;
+    this.incomeData = income;
+    this.expenditureData = expenditure;
+
+    // localStorage.setItem("assetsData", JSON.stringify(assets));
+    // localStorage.setItem("liabilitiesData", JSON.stringify(liabilities));
+    // localStorage.setItem("incomeData", JSON.stringify(income));
+    // localStorage.setItem("expenditureData", JSON.stringify(expenditure));
   }
 
-  liabFilter(sortedArray) {
-    var ledgerTree = JSON.parse(localStorage.getItem("LedgerTreeList"));
-    console.log(ledgerTree);
-    sortedArray = ledgerTree.filter(function(val)
-    {
-      console.log(val.Code);
-      console.log(val.GroupOrLedgerNo);
-      if (val.Code==null &&
-      val.GroupOrLedgerNo == 2)
-      return sortedArray;
-    });
-    localStorage.setItem("LedgerLiab", JSON.stringify(sortedArray));
-    return sortedArray;
+  recursiveLedger()
+  {
+    const assets=[this.assetsData];
+    let data = [];
+    console.log("This is assets data")
+    console.log(assets);
+    for (const val of assets){
+      console.log("%%%%%%%");
+      console.log(val);
+      console.log(val["Child"]);
+      if(val.hasOwnProperty('Child') && val["Child"])
+      {
+        console.log("&&&&&&&&");
+        console.log(val['Child']);
+        for(const val1 of val["Child"])
+        {
+          console.log("*********")
+          console.log(val1);
+          console.log(val1["Title"]);
+          if(val1.hasOwnProperty('Child') && val1["Child"])
+          {
+            for(const val2 of val1["Child"])
+            {
+              console.log("##############");
+              console.log(val2["Title"]);
+              if (val["TypeOf ===1"])
+              {
+                console.log("888888888")
+                console.log(val2["Title"]+" "+val2["Code"]);
+                data.push(val2["Title"]+" "+val2["Code"]);
+              }
+              if (val2.hasOwnProperty('Child') && val2["Child"])
+              {
+                for (const val3 of val2["Child"])
+                {
+                  console.log("00000000");
+                  console.log(val3["Title"]);
+                  data.push(val3["Title"]+" "+val3["Code"]);
+                  if (val3.hasOwnProperty('Child') && val3["Child"])
+                  {
+                    for (const val4 of val3["Child"])
+                    {
+                      console.log("111111111");
+                      console.log(val4["Title"]);
+                      data.push(val4["Title"]+" "+val4["Code"])
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    console.log(data);
   }
+
+  // assetsFilter(sortedArray) {
+  //   var ledgerTree = JSON.parse(localStorage.getItem("LedgerTreeList"));
+  //   console.log(ledgerTree);
+  //   sortedArray = ledgerTree.filter(function(val)
+  //   {
+  //     console.log(val.Code);
+  //     console.log(val.GroupOrLedgerNo);
+  //     console.log(val['Child'][0]['Title'])
+  //     console.log(val.Child[0].Child[0]);
+  //     localStorage.setItem("SubGroups",JSON.stringify(val.Child[0].Child[0]));
+  //     if (val.Code=="fgfg" &&
+  //     val.GroupOrLedgerNo == 1)
+  //     return sortedArray;
+  //   });
+  //   localStorage.setItem("LedgerAssets", JSON.stringify(sortedArray));
+  //   return sortedArray;
+  // }
+
+  // liabFilter(sortedArray) {
+  //   var ledgerTree = JSON.parse(localStorage.getItem("LedgerTreeList"));
+  //   console.log(ledgerTree);
+  //   sortedArray = ledgerTree.filter(function(val)
+  //   {
+  //     console.log(val.Code);
+  //     console.log(val.GroupOrLedgerNo);
+  //     if (val.Code==null &&
+  //     val.GroupOrLedgerNo == 2)
+  //     return sortedArray;
+  //   });
+  //   localStorage.setItem("LedgerLiab", JSON.stringify(sortedArray));
+  //   return sortedArray;
+  // }
+
 
   loadLedgerGroupList(): void 
   {
@@ -192,17 +290,4 @@ export class LandingLedgerComponent implements OnInit {
     this.expandedKeys = [];
   }
 
-  //New COA
-  onTabSelectn(e):void {
-    if (e.index == 0)
-    {
-      this.assetsFilter(e);
-      console.log(this.assetsFilter(e));
-    }
-    else if (e.index == 1)
-    {
-      this.liabFilter(e);
-      console.log(this.liabFilter(e));
-    }
-  }
 }
