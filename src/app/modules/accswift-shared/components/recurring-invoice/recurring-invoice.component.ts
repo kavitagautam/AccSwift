ca93cd12f177668ebcebf6be90f08a080c3dd51d
kaveeta <kavitagautam014@gmail.com>
2021-09-03 18:08:00 +0545

    Design Integration of Recurring Invoices

--- src/app/modules/accswift-shared/components/recurring-invoice/recurring-invoice.component.ts
+++ src/app/modules/accswift-shared/components/recurring-invoice/recurring-invoice.component.ts
@@ -8,9 +8,51 @@ import { BsModalRef } from 'ngx-bootstrap';
 })
 export class RecurringInvoiceComponent implements OnInit {
 
+  daily:boolean = true;
+  everyday:boolean = true;
+  weekly:boolean = false;
+  monthly:boolean = false;
+  yearly:boolean = false;
+  
   constructor(public modalRef:BsModalRef) {}
 
   ngOnInit() {
+  }
+
+  dailyReoccur():void 
+  {
+    this.daily = true;
+    this.everyday = true;
+    this.weekly = false;
+    this.monthly = false;
+    this.yearly = false;
+  }
+
+  weeklyReoccur():void 
+  {
+    this.daily = true;
+    this.everyday = false;
+    this.weekly = true;
+    this.monthly = false;
+    this.yearly = false;
+  }
+
+  monthlyReoccur():void 
+  {
+    this.daily = true;
+    this.everyday = false;
+    this.weekly = false;
+    this.monthly = true;
+    this.yearly = false;
+  }
+
+  yearlyReoccur():void 
+  {
+    this.daily = true;
+    this.everyday = false;
+    this.weekly = false;
+    this.monthly = false;
+    this.yearly = true;
   }
 
   onCancel():void {
