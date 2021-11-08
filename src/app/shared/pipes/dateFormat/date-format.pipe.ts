359dc599b05146b62f04d1828a40a989aecc57e4
kaveeta <kavitagautam014@gmail.com>
2021-11-08 15:41:05 +0545

    Issue in Date Format Solved

--- src/app/shared/pipes/dateFormat/date-format.pipe.ts
+++ src/app/shared/pipes/dateFormat/date-format.pipe.ts
@@ -1,44 +1,14 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { DatePipe, formatDate } from "@angular/common";
-import { SettingsService } from "@accSwift-modules/settings/services/settings.service";
-import { DATE_FORMAT } from "@accSwift-modules/settings/models/settings.model";
+import { DatePipe } from "@angular/common";
 
 @Pipe({
-  name: "preferredDateFormat"
+  name: "dateStringFormat"
 })
 export class DateFormatPipe implements PipeTransform {
-
-  dateFormats: DATE_FORMAT[];
-  selectedDateFormatId:any;
-
-  constructor(private settingsService: SettingsService) {
-    this.getDateFormat();
-    this.selectedDateFormatId = localStorage.getItem("SelectedDateFormat");
-  }
-
-  getDateFormat(): void {
-    this.settingsService.getDateFormats().subscribe((response) => {
-      this.dateFormats = response.Entity;
-      console.log(this.dateFormats);
-    });
-  }
-
-  transform(value: string, formatId: any) {
-    formatId = this.selectedDateFormatId;
-    console.log(value);
-    console.log(formatId);
-    console.log(this.dateFormats);
-    const dateFormat = this.dateFormats.filter((date) => {return date.ID == formatId});
-    console.log(dateFormat);
-    var datePipe = new DatePipe("en-US");
-    value = datePipe.transform(value, dateFormat[0].Format);
-    // value = formatDate(
-    //     value,
-    //     dateFormat[0].Format,
-    //     "en_US"
-    // );
-    console.log(value);
-    return value;
+  transform(value: string, format: string) {
+  var datePipe = new DatePipe("en-US");
+  value = datePipe.transform(value, format);
+  return value;
   }
   
 }
---
+++ src/app/shared/pipes/preferredDateFormat/preferred-date-format.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PreferredDateFormatPipe } from './preferred-date-format.pipe';
+
+describe('PreferredDateFormatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PreferredDateFormatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
